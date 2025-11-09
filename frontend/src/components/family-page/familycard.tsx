f299cfe623e1ac72ad681359c49f4f83c33cd954
st9phanie <guiragossianstephanie@gmail.com>
2025-11-09 22:24:08 +0200

    added families page ui with cards

--- frontend/src/components/family-page/familycard.tsx
+++ frontend/src/components/family-page/familycard.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import { Edit, Edit2, Edit3, Group, Trash, Trash2, Users, Users2 } from "lucide-react";
+import { Button } from "../ui/button";
 
 type Props = {
-    lastname:string | "Unnamed";
-    memberCount?: number;
+  lastname: string | "Unnamed";
+  memberCount?: number;
 }
 
-const FamilyCard = () => {
+const FamilyCard = ({ lastname, memberCount = 0 }: Props) => {
   return (
-    <div>
-        
+    <div className='flex flex-col rounded-2xl bg-lime-600 p-4 text-white h-[150px] w-[250px] gap-y-3 shadow-lg justify-between'>
+      <div>
+        <p className="text-xl font-bold tracking-wide drop-shadow-sm">
+          {lastname} Family
+        </p>
+
+        <div className="flex items-center mt-1 text-sm text-lime-100">
+          <Users className="w-4 h-4 mr-2" />
+          {memberCount === 0 ? (
+            <span>No members yet</span>
+          ) : (
+            <span>{memberCount} member{memberCount > 1 ? "s" : ""}</span>
+          )}
+        </div>
+      </div>
+
+
+      <div className="flex flex-row justify-between items-center mt-3 w-full">
+        <Button className="rounded-full cursor-pointer" size="icon-sm" variant="secondary"><Edit className="size-4 text-lime-900" /></Button>
+        <Button size="icon-sm" className="rounded-full cursor-pointer" variant="secondary"><Trash className="text-red-600 size-4" /></Button>
+      </div>
+
     </div>
   )
 }
--- frontend/src/pages/Families.tsx
+++ frontend/src/pages/Families.tsx
@@ -1,10 +1,12 @@
+import FamilyCard from '@/components/family-page/familycard'
 import Layout from '@/components/Layout'
 import { fetchUserFamilies } from '@/lib/functions'
+import { Plus, PlusCircle } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 
 const Families = () => {
     const [families, setFamilies] = useState([])
-    
+
     const loadFamilies = async () => {
         const data = await fetchUserFamilies(1)
         if (data) setFamilies(data)
@@ -17,10 +19,18 @@ const Families = () => {
     }, [])
 
     return (
-        <Layout>
-            {families.map((f,key)=>(
-                <p key={key}> {f.lastname} </p>
-            ))}
+        <Layout className='flex flex-col gap-y-5'>
+            <h1 className=' text-2xl drop-shadow-sm font-semibold tracking-wide'>Your Families</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 w-full'>
+                {families && families.map((f, key) => (
+                    <FamilyCard key={key} lastname={f.lastname} />
+
+                ))}
+                    <div className='flex cursor-pointer items-center rounded-2xl border-2 border-lime-600 p-4 text-white h-[150px] w-[250px] gap-y-3 shadow-lg justify-center'>
+                        <Plus className='text-lime-600 size-6' />
+                    </div>
+            </div>
+
         </Layout>
     )
 }
